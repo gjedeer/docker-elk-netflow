ec3cd6eb7793b4a95327fba8fd1c08ffac9174a9
GDR! <gdr@gdr-ThinkPad-X230.gdr.name>
2018-07-13 20:12:55 +0200

    Run setup hook for netflow plugin

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,5 @@ RUN CONF_DIR=/etc/elasticsearch gosu elasticsearch bin/elasticsearch-plugin \
 WORKDIR ${LOGSTASH_HOME}
 RUN gosu logstash bin/logstash-plugin install logstash-codec-netflow
 
-ADD logstash-30-netflow.conf /etc/logstash/conf.d/30-netflow.conf
+#ADD logstash-30-netflow.conf /etc/logstash/conf.d/30-netflow.conf
 ADD elk-post-hooks.sh /usr/local/bin/elk-post-hooks.sh
--- elk-post-hooks.sh
+++ elk-post-hooks.sh
@@ -9,5 +9,12 @@ echo "Running first-run setup hooks"
 touch /setup_complete
 
 cd /opt/logstash
-bin/logstash --modules netflow --setup
+service logstash stop
+
+re_is_numeric='^[0-9]+$'
+if ! [[ $NETFLOW_PORT =~ $re_is_numeric ]] ; then
+	NETFLOW_PORT=9995
+fi
+bin/logstash --modules netflow --setup -M netflow.var.input.udp.port=$NETFLOW_PORT
+
 echo "Netflow module setup complete"
